a4d3f5ea252035cbd332a27daeb4384ced0e6aa5
WindrunnerMax <651525974@qq.com>
2024-04-05 15:37:18 +0800

    24/04/05
    Former-commit-id: 096cd0f12a88208459bdfa892f5a2f8e38f628dc

--- Static/convert.js
+++ Static/convert.js
@@ -7,35 +7,35 @@ let s = `
 
 // document.querySelectorAll("dt").forEach(v => v.innerText = "`" + v.innerText + "`: ")
 
-s = s.replace(/[\n“”"‘’']/g, "")
-     .replace(/（/g, "(")
-     .replace(/）/g, ")")
-     .replace(/[,。]/g, "，")
-     .replace(/[；]/g, ";")
-     .replace(/[？]/g, "?")
-     .replace(/[！]/g, "!")
-     .replace(/[：]/g, ": ");
+s = s
+  .replace(/[\n“”"‘’']/g, "")
+  .replace(/（/g, "(")
+  .replace(/）/g, ")")
+  .replace(/[,。]/g, "，")
+  .replace(/[；]/g, ";")
+  .replace(/[？]/g, "?")
+  .replace(/[！]/g, "!")
+  .replace(/[：]/g, ": ");
 
 let start = false;
 const regexp = /[A-Za-z0-9\/\-\.\s@<>\{\}\[\]\?#:;_\+\\%\|=\$]/;
 const arr = s.split("");
 arr.forEach((v, i) => {
-    if(regexp.test(arr[i]) && !start){
-        start = true;
-        arr[i] = ("`"+arr[i]).replace(/[\s]/g, "");
-    }else if(!regexp.test(arr[i]) && start){
-        start = false;
-        arr[i] = ("`"+arr[i]).replace(/[\s]/g, "");
-        if(arr[i - 1]) arr[i - 1] = arr[i - 1].replace(/[\s]/g, "");
-    }
-    if(start){
-        arr[i] = arr[i].replace(/[，]/g, ", ");
-        // arr[i] = arr[i].replace(/[\s]/g, "");
-    } 
-})
+  if (regexp.test(arr[i]) && !start) {
+    start = true;
+    arr[i] = ("`" + arr[i]).replace(/[\s]/g, "");
+  } else if (!regexp.test(arr[i]) && start) {
+    start = false;
+    arr[i] = ("`" + arr[i]).replace(/[\s]/g, "");
+    if (arr[i - 1]) arr[i - 1] = arr[i - 1].replace(/[\s]/g, "");
+  }
+  if (start) {
+    arr[i] = arr[i].replace(/[，]/g, ", ");
+    // arr[i] = arr[i].replace(/[\s]/g, "");
+  }
+});
 
-
-if(arr.slice(-1).includes("，")) arr[arr.length-1] = "。";
+if (arr.slice(-1).includes("，")) arr[arr.length - 1] = "。";
 else arr.push("。");
 
 console.log(arr.join("").replace(/`，`/g, ", ").replace(/`，。/g, "`。"));
