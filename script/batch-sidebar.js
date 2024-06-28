9da2ba3ad916ae1baaee242d0a863fccff29c32f
WindrunnerMax <651525974@qq.com>
2024-06-28 23:16:05 +0800

    24/06/28
    Former-commit-id: bcd37178351dd846adfb4e085f708ee5a045bab1 [formerly 4064bef345150ea6eac5c72ea92dec2df8a4cd84]
    Former-commit-id: a780d2394aff90d74c6b19f3dcf75cf876022c5e

--- script/batch-sidebar.js
+++ script/batch-sidebar.js
@@ -6,6 +6,6 @@ const arr = str
   .split("\n")
   .filter((item) => item.trim() !== "")
   .map((line) => line.split("](")[1].replace(/%20/g, " "))
-  .map(item => item.replace(".md)", ""))
-  .map(item => `"${item}",`);
-arr.forEach(item => console.log(item));
+  .map((item) => item.replace(".md)", ""))
+  .map((item) => `"${item}",`);
+arr.forEach((item) => console.log(item));
