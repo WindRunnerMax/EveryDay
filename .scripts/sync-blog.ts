60aa6add2a3f6958f57db421dbd1a403e00fe342
WindRunnerMax <651525974@qq.com>
2024-12-22 16:19:32 +0800

    24/12/22

--- .scripts/sync-blog.ts
+++ .scripts/sync-blog.ts
@@ -17,7 +17,7 @@ const blog = path.resolve(root, `../Blog`);
     await exec(`rm -rf ${blog}/${group}`);
     await exec(`cp -r ${from} ${blog}`);
   }
-  
+
   console.log("Processing _sidebar.md");
   const body: string[] = [];
   for (const [key, value] of Object.entries(docs)) {
@@ -31,4 +31,56 @@ const blog = path.resolve(root, `../Blog`);
   }
   const content = body.join("\n");
   await fs.writeFile(path.join(blog, "_sidebar.md"), content);
+
+  console.log("Processing sitemap.xml");
+  const allPath: string[] = [];
+  Object.values(docs).forEach((value) => allPath.push(...value));
+  const getGitLastUpdatedTimeStamp = async (filePath: string) => {
+    let lastUpdated: number | null = null;
+    try {
+      const context = path.dirname(filePath);
+      const cmd = `cd ${context} && git log -1 --format=%at ${filePath}`;
+      const { stdout } = await exec(cmd);
+      lastUpdated = Number(stdout) * 1000;
+      if (Number.isNaN(lastUpdated)) lastUpdated = null;
+    } catch (e) {
+      /* noop */
+    }
+    return lastUpdated;
+  };
+  const domain = "https://blog.touchczy.top/";
+  const sitemap: string[] = [];
+  sitemap.push(`<?xml version="1.0" encoding="UTF-8"?>`);
+  sitemap.push(`<urlset`);
+  sitemap.push(`  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"`);
+  sitemap.push(`  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"`);
+  sitemap.push(
+    `  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9`
+  );
+  sitemap.push(`    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"`);
+  sitemap.push(`>\n`);
+
+  sitemap.push(`<url>`);
+  sitemap.push(`  <loc>${domain}</loc>`);
+  sitemap.push(`  <priority>1.0</priority>`);
+  sitemap.push(`</url>\n`);
+
+  sitemap.push(`<url>`);
+  sitemap.push(`  <loc>${domain}_sidebar.md</loc>`);
+  sitemap.push(`  <priority>1.0</priority>`);
+  sitemap.push(`</url>\n`);
+
+  for (const filePath of allPath) {
+    const file = path.join(blog, filePath + ".md");
+    console.log("Process File:", file);
+    const time = await getGitLastUpdatedTimeStamp(file);
+    sitemap.push(`<url>`);
+    sitemap.push(`  <loc>${domain}${filePath}.md</loc>`);
+    sitemap.push(`  <priority>1.0</priority>`);
+    time &&
+      sitemap.push(`  <lastmod>${new Date(time).toISOString()}</lastmod>`);
+    sitemap.push(`</url>\n`);
+  }
+  sitemap.push(`</urlset>`);
+  fs.writeFile(path.join(blog, "sitemap.xml"), sitemap.join("\n"));
 })();
