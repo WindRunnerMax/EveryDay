a3a7036a997b89f8dc9e39b1f266e2b979f7df64
WindRunnerMax <651525974@qq.com>
2025-03-15 11:27:35 +0800

    25/03/15

--- sidebar.ts
+++ sidebar.ts
@@ -267,7 +267,8 @@ export const sidebar: Sidebar = {
         "React/Hooks与事件绑定",
         "React/React闭包陷阱",
         "React/ReactPortals传送门",
-        "React/基于React的SSG渲染方案"
+        "React/基于React的SSG渲染方案",
+        "React/基于React的虚拟滚动方案"
       ]
     },
     {
