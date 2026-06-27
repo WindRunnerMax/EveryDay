fa05be9098740e6b2e5edf43379a1e6396448529
WindRunnerMax <651525974@qq.com>
2026-06-27 17:58:54 +0800

    26/06/27

--- sidebar.ts
+++ sidebar.ts
@@ -346,7 +346,8 @@ export const sidebar: Sidebar = {
         "RichText/React视图层适配器的模式扩展",
         "RichText/Immutable状态维护与增量渲染",
         "RichText/React可编辑节点的组件预设",
-        "RichText/React非编辑节点的内容渲染"
+        "RichText/React非编辑节点的内容渲染",
+        "RichText/编辑器历史变更管理与状态回溯"
       ]
     },
     {
